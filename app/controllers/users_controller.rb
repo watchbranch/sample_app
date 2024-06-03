42dba26b3e2ed838f2edd7c6aebf5be113e98a3f
watchtower <160295215+watchbranch@users.noreply.github.com>
2024-06-04 00:42:16 +0900

    account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
@@ -47,7 +46,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url, status: :see_other
   end
 
@@ -73,5 +72,4 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
-
 end
