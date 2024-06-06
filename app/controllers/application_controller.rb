769be4900a70e7208e4102b9784a4235f685f4e6
watchtower <160295215+watchbranch@users.noreply.github.com>
2024-06-06 22:38:41 +0900

    Add Micropost feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper
-    def hello
-        render html: "Hello, World!"
-    end
+  include SessionsHelper
+  def hello
+    render html: 'Hello, World!'
+  end
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, see: :see_other
+  end
 end
